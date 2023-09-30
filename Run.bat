e33c96fd777b59aacc3d4b502aefc26ae6c4f946
Black Ress V.O.I.D <138331417+theconcerman21@users.noreply.github.com>
2023-09-30 22:49:29 +0300

    Run.bat

---
+++ Run.bat
@@ -0,0 +1,21 @@
+@echo off
+echo Discord.py kütüphanesi yükleniyor...
+pip3 install discord
+
+echo Discord.ext.commands kütüphanesi yükleniyor...
+pip3 install discord.py
+
+echo googletrans kütüphanesi yükleniyor...
+pip3 install googletrans==4.0.0-rc1
+
+echo SQLAlchemy kütüphanesi yükleniyor...
+pip3 install sqlalchemy
+
+echo pysqlite3 kütüphanesi yükleniyor...
+pip3 install pysqlite3
+
+echo authlib kütüphanesi yükleniyor...
+pip3 install authlib
+
+echo Kütüphaneler başarıyla yüklendi.
+pause
